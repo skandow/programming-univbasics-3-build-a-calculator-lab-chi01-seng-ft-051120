e46f2b21cf9a37045dabd12d70d3d7f9dd8d6741
Steven Kandow <kando1sr@gmail.com>
2020-02-14 18:33:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  quotient
 end
 
 def multiplication(num1, num2)
-
+   product = num1 * num2
+   product
 end
 
 def modulo(num1, num2)
